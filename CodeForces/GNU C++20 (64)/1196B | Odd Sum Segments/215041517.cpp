ec9bbd31f0597f2289291c226f1c89e3291d40bf
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-10 12:47:26 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1196B | Odd Sum Segments/215041517.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define no return void (cout << "NO" << endl)
+#define yes return void (cout << "YES" << endl)
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr = 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+const int MAXN = 2e5 + 5;
+ 
+void burn () {
+    int n , k; cin >> n >> k;
+    vector <int> a(n);
+    vector <pair <int,int>> odd;
+    for (int i = 0;i < n; i++) {
+        cin >> a[i];
+        if (a[i] & 1) odd.emplace_back(a[i] , i + 1);
+    }
+    if (odd.size() < k || ((k & 1) && (odd.size() % 2 == 0)) || (odd.size() & 1) && (k % 2 == 0)) no;
+    vector <int> ans;
+ 
+    for (int i = 0; i < k; i++){
+        ans.emplace_back(odd.back().second);
+        odd.pop_back();
+    }
+    cout << "YES" << endl;
+    sort (all(ans));
+    ans.pop_back();
+    ans.push_back(n);
+    for (auto i : ans) cout << i << " ";
+    cout << endl;
+ 
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+    cin >> test_cases;
+    while (test_cases--) {
+        burn();
+        // solve ();
+    }
+    return 0;
+}
