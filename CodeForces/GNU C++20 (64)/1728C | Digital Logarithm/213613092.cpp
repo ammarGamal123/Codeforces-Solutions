aa63e811d920caf44d07c45b77b626ae11884192
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-10 12:48:42 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1728C | Digital Logarithm/213613092.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define rall(x) (x).rbegin(), (x).rend()
+//#define int    long long
+#define no return void (cout << "NO" << endl)
+#define yes return void (cout << "YES" << endl)
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+const int MAXN = 2e5 + 5;
+ 
+void burn () {
+    int n;
+    cin >> n;
+    int cost{};
+    vector<int> a(n), b(n);
+    for (auto &i: a) cin >> i;
+    for (auto &i: b) cin >> i;
+    priority_queue <int> pq_a (all(a)) , pq_b (all(b));
+ 
+    while (!pq_a.empty()){
+ 
+        if (pq_a.top() > pq_b.top()){
+            pq_a.push(to_string(pq_a.top()).size());
+            pq_a.pop();
+            cost ++;
+        }
+        else if (pq_a.top() < pq_b.top()){
+            pq_b.push(to_string(pq_b.top()).size());
+            pq_b.pop();
+            cost ++;
+        }
+        else {
+            pq_a.pop() , pq_b.pop();
+            continue;
+        }
+    }
+ 
+    cout << cost << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+        burn();
+        // solve ();
+    }
+    return 0;
+}
