1ee212124088b74ce8fb3ca359ab624beada356b
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:55:30 +0300

    Committing files

---
+++ CodeForces/GNU C++17/456B | Fedya and Maths/208359362.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+ 
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 5 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+int is_Prime (int n) {
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+vector<long long> trial_division3(long long n) {
+    vector<long long> factorization;
+    for (int d : {2, 3, 5}) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    static array<int, 8> increments = {4, 2, 4, 2, 4, 6, 2, 6};
+    int i = 0;
+    for (long long d = 7; d * d <= n; d += increments[i++]) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+        if (i == 8)
+            i = 0;
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    getunique(factorization);
+    return factorization;
+}
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while(power > 0) {
+ 
+        if(power & 1 ) { // Can also use (power & 1) to make code even faster
+            result = (result*base) % Mod;
+        }
+        base = (base * base) % Mod;
+        power = power / 2; // Can also use power >>= 1; to make code even faster
+    }
+    return result;
+}
+ 
+void burn () {
+    int code, x, n;
+    string s;
+    getline(cin, s);
+    n = s.length();
+    if (n < 2) {
+        n = 2;
+        s = "0" + s;
+    }
+ 
+    x = stoi(s.substr(n - 2));
+ 
+    if (x % 4 == 0) {
+        cout << 4 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+ 
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+    // cin >> test_cases;
+    while (test_cases--) {
+        burn();
+    }
+    return 0;
+}
