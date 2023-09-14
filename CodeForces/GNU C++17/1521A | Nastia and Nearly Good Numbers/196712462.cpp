5f9d1a786bf0512371db8ee5d69e6b2472088e26
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:02:06 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1521A | Nastia and Nearly Good Numbers/196712462.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int   long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr),cout.tie(nullptr);
+ 
+void solve (){
+    int a , b;
+    cin >> a >> b;
+    int x = a * b;
+    int y = a;
+    if (b == 1){
+        cout << "NO" << endl;
+        return;
+    }
+    if (a == b || b == 1)
+        y = b * Mod;
+    cout << "YES" << endl;
+    cout << x << " " << y << " " << x + y << endl;
+ 
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
