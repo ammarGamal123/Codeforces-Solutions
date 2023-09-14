f0815709af3f9f97e990b2c0ae89512badbca835
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:53:58 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1573B | Swaps/209875979.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+/*
+ *                _,.---.---.---.--.._
+           _.-' `--.`---.`---'-. _,`--.._
+          /`--._ .'.     `.     `,`-.`-._\
+         ||   \  `.`---.__`__..-`. ,'`-._/
+    _  ,`\ `-._\   \    `.    `_.-`-._,``-.
+ ,`   `-_ \/ `-.`--.\    _\_.-'\__.-`-.`-._`.
+(_.o> ,--. `._/'--.-`,--`  \_.-'       \`-._ \
+ `---'    `._ `---._/__,----`           `-. `-\
+           /_, ,  _..-'                    `-._\
+           \_, \/ ._(
+            \_, \/ ._\
+             `._,\/ ._\
+               `._// ./`-._
+                 `-._-_-_.-'
+ 
+*/
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+ 
+void burn () {
+    int n; cin >> n;
+    vector < pair <int,int> > a (n) , b (n);
+    for (int i = 0;i < n ; i ++){
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+    for (int i = 0;i < n;i++){
+        cin >> b[i].first;
+        b[i].second = i + 1;
+    }
+    sort (all(a)) , sort (all(b));
+    int cur = INT_MAX , ans = INT_MAX;
+    for (int i = n - 1; i >= 0; i--){
+        cur = min (cur , b[i].second);
+        ans = min (ans , a[i].second + cur - 2);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+    cin >> test_cases;
+    while (test_cases--) {
+ 
+        burn();
+    }
+    return 0;
+}
