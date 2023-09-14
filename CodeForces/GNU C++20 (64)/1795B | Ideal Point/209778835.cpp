380987ed3e9bd2cae6d32e50bf4c70444cf362a5
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:54:39 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1795B | Ideal Point/209778835.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+/*
+ *                _,.---.---.---.--.._
+           _.-' `--.`---.`---'-. _,`--.._
+          /`--._ .'.     `.     `,`-.`-._\
+         ||   \  `.`---.__`__..-`. ,'`-._/
+    _  ,`\ `-._\   \    `.    `_.-`-._,``-.
+ ,`   `-_ \/ `-.`--.\    _\_.-'\__.-`-.`-._`.
+(_.o> ,--. `._/'--.-`,--`  \_.-'       \`-._ \
+ `---'    `._ `---._/__,----`           `-. `-\
+           /_, ,  _..-'                    `-._\
+           \_, \/ ._(
+            \_, \/ ._\
+             `._,\/ ._\
+               `._// ./`-._
+                 `-._-_-_.-'
+ 
+*/
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1e7 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+ 
+void burn () {
+    int n , k;
+    cin >> n >> k;
+    vector <int> part(55 , 0);
+    for (int i = 0; i < n;i++){
+        int l , r;
+        cin >> l >> r;
+        if (l <= k && k <= r)
+            part[l] ++ , part[r + 1] --;
+    }
+    for (int i = 2; i <= 55;i++){
+        part[i] += part[i - 1];
+    }
+    int check = part[k];
+    for (int i = 1; i < 55; i++){
+        if (k != i){
+            if (check <= part[i])
+                return void (cout << "NO" << endl);
+        }
+    }
+    cout << "YES" << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+ 
+        burn();
+    }
+    return 0;
+}
