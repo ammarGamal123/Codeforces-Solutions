91524f0f8e922d9a91b2d18fe171dbe38000ee88
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:57:30 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1829F | Forever Winter/206193280.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, MOD = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 1e7 + 5 , OO = 0x3F3F3F3F;
+ 
+ 
+void solve (){
+ 
+    int n , m;
+    cin >> n >> m;
+ 
+    int cnt [n + 1] = {0};
+ 
+    for (int i = 0; i < m;i++){
+        int x , y;
+        cin >> x >> y;
+        cnt[x] ++;
+        cnt [y] ++;
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (cnt[i] == 1)
+            sum++;
+    }
+    cout << m - sum << " " << sum / (m - sum) << endl;
+}
+ 
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
