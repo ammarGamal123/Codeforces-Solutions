9f5a34a31906f1c539cf8be1005fc11d04f596ea
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:07:32 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1426C | Increase and Copy/190373514.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int  long long
+#define lll __int128
+//#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 1000001, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(0),cout.tie(0);
+void solve () {
+    int n;
+    cin >> n;
+    int ans = (int) 1e9;
+    for (int i = 1; i * i <= n;i++){
+        ans = min (ans , (i - 1) + (n + i - 1 ) / i - 1);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
