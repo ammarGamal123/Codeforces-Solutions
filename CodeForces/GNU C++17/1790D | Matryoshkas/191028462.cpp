e4739c7f6a517bb52cf41a25af4ee7184b73f616
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:05:54 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1790D | Matryoshkas/191028462.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int  long long
+#define lll __int128
+//#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 1000001, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(0),cout.tie(0);
+void solve (){
+    int n;
+    cin >> n;
+    multiset<int> ms;
+    for (int i = 0;i < n;i++){
+        int x;
+        cin >> x;
+        ms.insert(x);
+    }
+    int ans {};
+    while (ms.size()){
+        ans ++;
+        int cur = *ms.begin();
+        while (ms.count(cur)){
+            ms.erase(ms.find(cur));
+            ++cur;
+        }
+    }
+    cout << ans <<  endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
