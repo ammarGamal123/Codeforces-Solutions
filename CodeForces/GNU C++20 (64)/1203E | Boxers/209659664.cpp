33debc2dfc354eb808ef88e489621060b25f274b
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:54:48 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1203E | Boxers/209659664.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+#define endl '\n'
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1e7 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+ 
+void burn () {
+    int n; cin >> n;
+    vector <int> a (n);
+    for (auto &i : a ) cin >> i;
+    sort (a.begin() , a.end());
+    int ans {};
+    map <int,int> cur;
+    for (int i = 0;i < n;i++){
+        if (!cur.count (a[i] - 1) && a[i] != 1){
+            cur[a[i] - 1] ++;
+            ans ++;
+        }
+        else if (!cur.count(a[i])) {
+                cur[a[i]] ++;
+                ans ++;
+        }
+        else if (!cur.count(a[i] + 1)){
+            cur[a[i] + 1] ++;
+            ans ++;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+    // cin >> test_cases;
+    while (test_cases--) {
+        burn();
+    }
+    return 0;
+}
