dc1c176467a34dcbec3f035f38b15d2e14efc1cd
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:58:57 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1665B | Array Cloning Technique/204372153.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 7000000 , OO = 0x3F3F3F3F;
+ 
+ 
+void solve () {
+    int n;
+    cin >> n;
+ 
+    vector <int> a (n);
+ 
+    map <int,int> mp;
+ 
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+ 
+    if (mp.size() == 1){
+        cout << 0 << endl;
+        return;
+    }
+    int Max = {};
+ 
+    for (auto i : mp){
+        Max = max (Max , i.second);
+    }
+ 
+    int ans {};
+ 
+    while (Max < n){
+        int diff = min (n - Max , Max );
+        ans += 1 + diff;
+        Max += diff;
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
