9ebfa02660c0c67054293efaa3c3fe14dd0b1cf2
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:07:01 +0300

    Committing files

---
+++ CodeForces/GNU C++17/977B | Two-gram/190687340.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int  long long
+#define lll __int128
+//#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 1000001, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(0),cout.tie(0);
+ 
+void solve (){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map < string , int > mp;
+    for (int i = 0;i + 1 < s.size();i ++){
+        string cur = "" ;
+        cur += s[i] , cur += s[i + 1];
+        mp[cur] ++;
+    }
+    string ans = "";
+    int mx = 0;
+    for (auto i : mp){
+        if (i.second >= mx){
+            ans = i.first;
+            mx = i.second;
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    //cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
