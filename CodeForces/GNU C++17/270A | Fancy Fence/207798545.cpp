23d67d5793f37ffb18a4d038b0a46ff5182af269
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:56:20 +0300

    Committing files

---
+++ CodeForces/GNU C++17/270A | Fancy Fence/207798545.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 10000007 , inf = 1e9 , bitstr= 27;
+const int NN = 1e7 + 5 , OO = 0x3F3F3F3F;
+ 
+void burn () {
+ 
+    int a;
+    cin >> a;
+ 
+    if (360 % (180 - a) == 0) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+ 
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+      cin >> test_cases;
+    while (test_cases--) {
+        burn();
+    }
+    return 0;
+}
