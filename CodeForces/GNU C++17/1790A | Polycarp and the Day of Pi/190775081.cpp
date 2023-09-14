2a724ea744bb6ff1cabc53e073a5a3ab574188eb
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:06:14 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1790A | Polycarp and the Day of Pi/190775081.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int  long long
+#define lll __int128
+//#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 1000001, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(0),cout.tie(0);
+void solve (){
+    string origin = "314159265358979323846264338327";
+    string s;
+    cin >> s;
+    bool valid = true;
+    int cnt {};
+    for (int i = 0;i < origin.size(); i++){
+        if (s[i] != origin[i])
+        {
+            valid = false;
+            break;
+        }
+        else cnt ++;
+    }
+    cout << cnt << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
