2b7db47e35db8f0be0664095ee43c310ae708df6
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:59:59 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1335A | Candies and Two Sisters/201987379.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+void solve (){
+    int n;
+    cin >> n;
+    if (!n || n == 1 || n == 2){
+        cout << 0 << endl;
+    }
+    else {
+        if (n & 1){
+            cout << n / 2 << endl;
+        }
+        else {
+            cout << (n - 1) / 2 << endl;
+        }
+    }
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
