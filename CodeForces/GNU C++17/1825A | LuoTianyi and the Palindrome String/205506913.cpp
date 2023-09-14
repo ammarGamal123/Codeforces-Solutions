27c1728a3624202e53a7aa2299189abe2e19a708
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:58:35 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1825A | LuoTianyi and the Palindrome String/205506913.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 7000000 , OO = 0x3F3F3F3F;
+ 
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+int solve () {
+ 
+    string s;
+    cin >> s;
+    set <char> st;
+    for (auto i : s)
+        st.insert (i);
+    if (st.size() == 1){
+        cout << -1 << endl;
+    }
+    else cout << s.size() - 1 << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+      cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
