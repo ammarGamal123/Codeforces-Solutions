0904b4e7b79f81b2b8578748034a004cd722e6a0
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:58:12 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1691B | Shoe Shuffling/205540854.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 7000000 , OO = 0x3F3F3F3F;
+ 
+void solve () {
+ 
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    map <int,int> mp;
+ 
+    for (auto &i : a) {
+        cin >> i;
+        mp[i] ++;
+    }
+ 
+    for (auto i : mp){
+        if (i.second == 1){
+            cout << -1 << endl;
+            return;
+        }
+    }
+ 
+    int cur = 1;
+ 
+    for (auto i : mp){
+        for (int j = 0; j < i.second - 1; j++){
+            cout << cur + j + 1 << " ";
+        }
+        cout << cur << " ";
+        cur += i.second;
+    }
+    cout << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+      cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
