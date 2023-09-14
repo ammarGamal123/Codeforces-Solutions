a9323dfba18a8aae52cf2e7214e2a834d3e3e827
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:59:56 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1335B | Construct the String/201992250.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+void solve (){
+    int n , a , b;
+    cin >> n >> a >> b;
+    string s = "";
+    char c = 'a';
+    for (int i = 0;i < n; i++){
+        s += char ('a' + i % b);
+    }
+    cout << s << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
