beb726e2c402b7d67a335e7a0cfd4e731a4f9593
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:00:23 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1811A | Insert Digit/200788218.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+ 
+void solve () {
+    int n , d;
+    cin >> n >> d;
+    string s;
+    cin >> s;
+    int idx = 0;
+    while (idx < n && s[idx] - '0' >= d){
+        ++idx;
+    }
+    if (idx == n){
+        cout << s << d << endl;
+    }
+    else {
+        s.insert(idx , to_string(d));
+        cout << s << endl;
+    }
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+ 
+     cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
