3fed24083b5f40e990ef5c2015e7dc7601533bbb
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:55:18 +0300

    Committing files

---
+++ CodeForces/GNU C++17/235A | LCM Challenge/208366359.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int    long long
+#define lll __int128
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+ 
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 5 , inf = 1e9 , bitstr= 27;
+const int NN = 1000 + 5 , OO = 0x3F3F3F3F;
+ 
+int gcd(int a ,int b){
+    while(b != 0){int curr = a ; a = b; b = curr % b;}
+    return a;
+}
+int lcm (int a , int b){
+    return (a / gcd(a,b)) * b;
+}
+void burn () {
+    int n;
+    cin >> n;
+    int ans = 0;
+    int t = min (50LL , n);
+    for (int i = 0; i < t; i++){
+        for (int j = 0; j < t; j++){
+            for (int k = 0; k < t; k++){
+                ans = max (ans , lcm (n - i , lcm (n - j , n - k)));
+            }
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , x);
+   //  cin >> test_cases;
+    while (test_cases--) {
+        burn();
+    }
+    return 0;
+}
