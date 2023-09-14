ad935ce213e5f2cfc8402f3b385e395467bbc1a4
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 15:04:23 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1704C | Virus/193459908.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define int  long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+const long long INF = 1ll << 32;
+const long double PI = acos(-1);
+const int N = 1000001, Mod = 1000000007 , inf = 1e9 , bits = 27;
+const int NN = 1e9 , OO = 0x3F3F3F3F;
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr),cout.tie(nullptr);
+void solve () {
+    int n , m;
+    cin >> n >> m;
+    vector <int> a(m) , b;
+    for (int i = 0; i < m; i++){
+        cin >> a[i];
+    }
+    sort (all(a));
+    b.emplace_back((a.front() - 1) + (n - a.back()));
+    for (int i = 1; i < m; i++){
+        b.emplace_back(a[i] - a[i - 1] - 1);
+    }
+    sort (rall(b));
+    int protect {};
+    for (int i = 0;i < b.size(); i++){
+        b[i] -= (2 * i);
+        b[i] = max (0LL , b[i] - (i * 2LL));
+        protect += b[i] - (b[i] > 1);
+    }
+    cout << n - protect << endl;
+}
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
