cb4762603f0767ba3d64aeaa769eeecfef714f9c
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-14 14:59:11 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1821A | Matching/204079852.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using int64 = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbeg    in(), (x).rend()
+#define int    long long
+#define lll __int128
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define endl '\n'
+#define NeedForSpeed ios_base::sync_with_stdio(false) , cin.tie(nullptr), cout.tie(nullptr);
+const int64 INF = 1'000'000'000LL + 100;
+const long double PI = acos(-1);
+const int N1 = 2e5 + 7, Mod = 1000000007 , inf = 1e9 , bitstr= 27;
+const int NN = 7000000 , OO = 0x3F3F3F3F;
+void solve () {
+    string s;
+    cin >> s;
+    int ans = 1;
+    if (s[0] == '0'){
+        cout << 0 << endl;
+        return;
+    }
+    if (s[0] == '?'){
+        ans = 9;
+    }
+ 
+    for (int i = 1; i < s.size(); i++){
+        if (s[i] == '?') ans *= 10;
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int32_t main() {
+    NeedForSpeed
+    int test_cases = 1 , Case = 1;
+    // freopen("file_name.in" , "r" , stdin);
+    // freopen("file_name.out" , "w" , stdout);
+     cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+    return 0;
+}
